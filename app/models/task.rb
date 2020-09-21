23616d356930e25b7c218e7c025650848775bbf4
yamabook37 <yuki.yamamoto0711@gmail.com>
2020-09-22 01:22:41 +0900

    #4-5 ユーザcontrollerの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+    #検証エラーを発見すると、errorsにエラー内容を格納する
+  end
 end
