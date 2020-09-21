def085b7e83f95efe6fdcade3890786ef2996c12
yamabook37 <yuki.yamamoto0711@gmail.com>
2020-09-21 21:55:58 +0900

    #3 edit,show,destroyの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -10,5 +12,31 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク 「#{task.name}」を更新しました。"
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    flash[:notice] =  "タスク 「#{task.name}」を登録しました。"
+    redirect_to tasks_url
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク 「#{task.name}」を削除しました。"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
